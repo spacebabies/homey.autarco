bfc18faf197765c7992c64a0e17847dbd916f7bd
Joost Baaij <joost@spacebabies.nl>
2023-01-19 11:11:26 +0100

    added Device for My Autarco inverter

---
+++ drivers/my-autarco/driver.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { Driver } = require('homey');
+
+class MyDriver extends Driver {
+
+  /**
+   * onInit is called when the driver is initialized.
+   */
+  async onInit() {
+    this.log('MyDriver has been initialized');
+  }
+
+  /**
+   * onPairListDevices is called when a user is adding a device
+   * and the 'list_devices' view is called.
+   * This should return an array with the data of devices that are available for pairing.
+   */
+  async onPairListDevices() {
+    return [
+      {
+        name: 'My Autarco',
+        data: {
+          id: 'my-autarco',
+        },
+      },
+    ];
+  }
+
+}
+
+module.exports = MyDriver;
